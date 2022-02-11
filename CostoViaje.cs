4af60ba2c917f4c54425bb62d15632313d008689
Pedro Gonzalez <pedrogcernuda@gmail.com>
2022-02-10 21:22:51 -0600

    cambio de nombre de archivo

---
+++ CostoViaje.cs
@@ -0,0 +1,25 @@
+//Autor: Pedro Luis González Cernuda
+//Programa que calcula el costo del viaje que efectua un automovil de acuerdo a los datos proporcionados.
+//Se solicita que se ingrese la marca, la distancia a recorrer, la eficiencia de consumo del vehículo y el tipo de gasolina.
+using System;
+
+namespace CostoViaje
+{
+    public class Program
+    {
+        //Constantes del precio de gasolina magna y premium.
+        static double PRECIO_MAGNA = 19.19;
+        static double PRECIO_PREMIUM = 21.49;
+
+        public static void Main(string[] args)
+        {
+            //Declaración de variables.
+            string marca, tipoGas;
+            double distancia, eficiencia;
+
+            Console.WriteLine("COSTO DE UN VIAJE");
+            Console.WriteLine($"Marca del carro: {marca}");
+            
+        }
+    }
+}
