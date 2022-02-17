19a3e115858fca8a4a786edee1d1f9dff7002615
Pedro Gonzalez <pedrogcernuda@gmail.com>
2022-02-17 15:41:01 -0600

    mapa mental de .net

--- Practica_2/PlanRenta.cs
+++ Practica_2/PlanRenta.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+namespace Practica2{
+    public class PlanRenta
+    {
+        public string NombreUsuario {get; set;}
+        public int MinutosConsumidos {get; set;}
+        public int MbConsumidos {get; set;}
+        public int TipoCelular {get; set;}
+
+        public PlanRenta(string NombreUsuario,int MinutosConsumidos,int MbConsumidos,int TipoCelular)
+        {
+            this.NombreUsuario = NombreUsuario;
+            this.MinutosConsumidos = MinutosConsumidos;
+            this.MbConsumidos = MbConsumidos;
+            this.TipoCelular = TipoCelular;
+        }
+
+        
+    }
+}
