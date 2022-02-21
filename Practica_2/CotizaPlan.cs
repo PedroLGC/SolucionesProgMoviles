c46768b796031282be3869a914f411ae40870878
Pedro Gonzalez <pedrogcernuda@gmail.com>
2022-02-21 15:15:25 -0600

    FinalizaciÃn de programa

--- Practica_2/CotizaPlan.cs
+++ Practica_2/CotizaPlan.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿
+using System;
 
 namespace Practica2
 {
+    /*
+    * Autor: Pedro Luis González Cernuda
+    *
+    * Programa que hace una cotización de un plan mensual de telefonía movil. 
+    * El programa pide al cliente su nombre y la gama de celular que contrató 
+    * y devuelve los megabytes y minutos consumidos y el pago a realizar.
+    */
     public class CotizaPlan
     {
         public static void Main(string[] args)
         {
-            
+            string usuario = " ";
+            int gama = 0;
+            bool ciclo = true;
+            PlanRenta plan;
+            Console.WriteLine("COTIZACIÓN DE PLAN DE RENTA");
+            do
+            {
+                ciclo = true;
+                Console.Write("Introduzca su nombre: ");
+                usuario = Console.ReadLine();
+                if (usuario.Length > 1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("El campo no puede ir vacio, vuelva a introducir el dato");
+                    ciclo = false;
+                }
+            } while (!ciclo);
+
+            do
+            {
+                ciclo = true;
+                Console.Write("Introduzca la gama de su dispositivo 1.Gama baja 2.Gama media 3.Gama alta: ");
+                try
+                {
+                    gama = Int32.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"Dato inválido, vuelva a introducir el dato: {e.Message}");
+                    ciclo = false;
+                }
+                if (gama < 1 || gama > 3)
+                {
+                    Console.WriteLine($"Solo puede elegir la opcion 1, 2 o 3");
+                    ciclo=false;
+                }
+            } while (ciclo == false);
+
+            plan = new PlanRenta(usuario, gama);
+            plan.ConsumeMB(1000);
+            plan.ConsumeMinutos(900);
+            Console.Write(plan.ToString());
         }
     }
 }
--- Practica_2/PlanRenta.cs
+++ Practica_2/PlanRenta.cs
@@ -1,22 +1,76 @@
 ﻿using System;
 
-namespace Practica2 
+namespace Practica2
 {
+    /*
+    * Autor: Pedro Luis González Cernuda
+    *
+    * Clase que contiene los métodos para calcular
+    * el costo del plan telefónico y el método de impresión.
+    */
     public class PlanRenta
     {
-        public string NombreUsuario {get; set;}
-        public int MinutosConsumidos {get; set;}
-        public int MbConsumidos {get; set;}
-        public int TipoCelular {get; set;}
+        public string NombreUsuario { get; set; }
+        public int MinutosConsumidos { get; set; }
+        public int MbConsumidos { get; set; }
+        public int TipoCelular { get; set; }
 
-        public PlanRenta(string NombreUsuario,int MinutosConsumidos,int MbConsumidos,int TipoCelular)
+        public PlanRenta(string NombreUsuario, int TipoCelular)
         {
             this.NombreUsuario = NombreUsuario;
-            this.MinutosConsumidos = MinutosConsumidos;
-            this.MbConsumidos = MbConsumidos;
+            this.MinutosConsumidos = 0;
+            this.MbConsumidos = 0;
             this.TipoCelular = TipoCelular;
         }
-        
-        
+
+        public void ConsumeMinutos(int min)
+        {
+            this.MinutosConsumidos += min;
+        }
+        public void ConsumeMB(int mb)
+        {
+            this.MbConsumidos += mb;
+        }
+        public double CalculaPagoMensual()
+        {
+            double resultado;
+
+            if (MinutosConsumidos >= 800 && MbConsumidos >= 1000)
+            {
+                resultado = 199 + (MinutosConsumidos - 800) * 0.25 + (MbConsumidos - 1000) * 0.35;
+            }
+            else if (MinutosConsumidos >= 800)
+            {
+                resultado = 199 + (MinutosConsumidos - 800) * 0.25;
+            }
+            else
+            {
+                resultado = 199 + (MbConsumidos - 1000) * 0.35;
+            }
+            if (resultado > 0)
+                return resultado;
+            return 0;
+        }
+    public double DiferenciaEquipo(int selector)
+    {
+        if (selector == 1)
+        {
+            return 0;
+        }
+        else if (selector == 2)
+        {
+            return 1999;
+        }
+        else
+        {
+            return 5999;
+        }
     }
+    public override string ToString()
+    {
+        return "\nRESULTADOS \n" +
+        $"\nUsuario: {NombreUsuario} \nMinutos consumidos: {MinutosConsumidos} \nMB consumidos: {MbConsumidos} \n" +
+         $"Renta mensual: {CalculaPagoMensual()} \nPago diferencial de su equipo: ${DiferenciaEquipo(TipoCelular)}";
+    }
+}
 }
